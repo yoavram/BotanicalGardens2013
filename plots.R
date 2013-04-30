87bb2a01bdea1383639881152d1f97dc14058aea
Yoav Ram <yoavram+github@gmail.com>
2013-04-30 15:13:24 +0300

    added msb plot

--- plots.R
+++ plots.R
@@ -1,10 +1,20 @@
 library(ggplot2)
 U = 0.0004
 s = 0.01
-data <- -log(dpois(seq(0,3), U/s))
 
 theme_set(theme_gray(24))
 
+data <- -log(dpois(seq(0,3), U/s))
 qplot(x=seq(0,3), y=data, geom="bar", stat="identity", fill=I("white"), color=I("black")) + 
   labs(x="מס' מוטציות שליליות", y="לוג שכיחות") +
   scale_y_continuous(labels = c())
+
+
+fitness <- function(g) {
+  return(exp(-U*(1-(1-s)^g)))
+}
+
+gens = seq(2000)
+qplot(x=gens, y=sapply(gens, fitness)) + 
+  labs(x="דור", y="כשירות ממוצעת באוכלוסיה") +
+  scale_y_continuous(breaks = c(1, 1-U), labels=c(1,1-U))
